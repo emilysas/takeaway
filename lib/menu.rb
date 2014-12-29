a3877bd0e701fd6251c9841b703593284cf205a5
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-29 13:14:55 +0000

    tests passed: can show price of menu item; will raise error if item not on menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,8 +4,12 @@ class Menu
     @menu = [{name: "onion bhaji", price: 2.45}, {name: "vegetable somosa", price: 2.45}, {name: "garlic mushrooms", price: 2.50}, {name: "aloo chat", price: 2.60}, {name: "paneer tikka", price: 3.95}, {name: "paneer shashlik", price: 6.25}, {name: "vegetable balti", price: 6.75}, {name: "vegetable roshney", price: 5.85}, {name: "vegetable pasanda", price: 5.85}, {name: "vegetable jalfrezi", price: 5.85}, {name: "vegetable damask", price: 5.35}, {name: "vegetable pathia", price: 5.35}, {name: "vegetable korai", price: 5.85}, {name: "vegetable muglai", price: 5.95}, {name: "vegetable korma", price: 5.35}]
   end
 
-  def show_price(menu_item)
-    @menu[name: menu_item][:price]
+  def show_price(choice)
+    if takeaway_item = @menu.find{|menu_item| menu_item[:name] == choice }
+      return takeaway_item[:price]
+    else
+      raise "Sorry, we do not sell this item"
+    end
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,7 +4,11 @@ describe Menu do
   let(:menu){Menu.new}
   
   it 'can show the price of a menu item' do
-    expect(menu.show_price(name: "vegetable_bhaji")).to eq(2.95)
+    expect(menu.show_price("onion bhaji")).to eq(2.45)
+  end
+
+  it 'can inform the customer if a menu item doesn\'t exist' do
+    expect{menu.show_price("chocolate")}.to raise_error(RuntimeError, "Sorry, we do not sell this item")
   end
 
 end
