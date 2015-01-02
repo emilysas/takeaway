eab73ff337c7a96f132c849ee68f50355bc8bfbc
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-02 20:35:45 +0000

    refactoring, deleting tests will rewrite

--- lib/order.rb
+++ lib/order.rb
@@ -1,19 +1,13 @@
 require 'rubygems'
 require 'twilio-ruby'
-require 'time'
 require 'menu'
-
-#What does this class do?
-#1. It receives an order from a customer
-#2. It asks the customer to estimate the total
-#3. It calculates the total of the order
-#4. It compares this total to the total estimated
-#5. It calculates the delivery time
-#6. It sends a text
+require 'total'
+require 'text'
 
 class Order
 
   include Total
+  include Text
 
   attr_reader :order
 
@@ -24,6 +18,7 @@ class Order
   def take_order 
     ask_customer
     get_total
+    correct? ? send_text : recalculate_total
   end
 
   def ask_customer
@@ -32,7 +27,7 @@ class Order
     note
   end
 
-  def note
+  def note_down
     while !order.empty? do
       menu.stocked?(order) ? @order << order
       ask_customer
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -10,21 +10,19 @@ describe Order do
   end
 
   xit 'can inform the customer if a menu item doesn\'t exist' do
-    expect{order.show_price("chocolate")}.to raise_error(RuntimeError, "Sorry, we do not sell this item")
+
   end
 
   xit 'can take an order' do
-    order.take_order
-    expect(order.show_order).to
+
   end
 
   xit 'will ask the customer to estimate the total' do
-    order.take_order
-    expect(order).to receive(:get_total)
+
   end
 
   xit 'can calculate the order total' do
-    expect(order.calculate_total).to eq(2.45)
+
   end
 
   xit 'can send a text to the customer' do
