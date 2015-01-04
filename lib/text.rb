36a2633d40bec3b4032508b6b0f89ef116570f51
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-04 13:53:18 +0000

    takeaway application working, and sending texts, but will only allow you to order 1 item

--- lib/text.rb
+++ lib/text.rb
@@ -3,16 +3,15 @@ require 'time'
 
 module Text
 
-  # include Twilio
-  # include Time
+  include Twilio
 
-  # def delivery_time
-  #   t = Time.now + (60*60)
-  #   time = "#{t.hour}:#{t.min}"
-  # end
+  def delivery_time
+    t = Time.now + (60*60)
+    time = "#{t.hour}:#{t.min}"
+  end
 
-  # def send_text
-  #   send_sms("Thank you for your order, this should be delivered to you by #{delivery_time}")
-  # end 
+  def send_text
+    send_sms("Thank you for your order, this should be delivered to you by #{delivery_time}")
+  end 
 
 end
--- lib/twilio.rb
+++ lib/twilio.rb
@@ -3,17 +3,16 @@ require 'twilio-ruby'
 
 module Twilio
 
+def send_sms(text)
   account_sid = 'ACd44c89dbeb30e2dffb63bc5efcd8ff13'
   auth_token = ENV['TWILIO_KEY']
   @client = Twilio::REST::Client.new account_sid, auth_token
-  
-  def send_sms(text)
-    message = @client.account.messages.create(
-    :body => text,
-    :to => ENV['TWILIO_NUM'],
-    :from => "+441871851019"
-    )
-    puts message.sid
-  end
+  message = @client.account.messages.create(
+  :body => text,
+  :to => ENV['TWILIO_NUM'],
+  :from => "+441871851019"
+  )
+  puts message.sid
+end
 
 end
